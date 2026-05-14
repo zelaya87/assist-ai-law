e69261415b112c95a8d9f2252d00bad8e551c804
zelaya87 <zetechnosinformatica@gmail.com>
2026-05-14 19:47:01 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,14 @@
-import { createStartHandler, defaultStreamHandler } from '@tanstack/react-start/server'
-import { getRouter } from './router'
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-export default createStartHandler({
-  createRouter: getRouter,
-})(defaultStreamHandler)
+export const getRouter = () => {
+  const queryClient = new QueryClient();
+  const router = createRouter({
+    routeTree,
+    context: { queryClient },
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  });
+  return router;
+};
