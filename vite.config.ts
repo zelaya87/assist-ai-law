7d6f1cb68d4e27b8db67e6e9a442f8d9538e41b2
zelaya87 <zetechnosinformatica@gmail.com>
2026-05-14 19:34:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackStart } from "@tanstack/react-start/plugin";
-import tailwindcss from "@tailwindcss/vite";
-import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { nitro } from 'nitro/vite';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tsConfigPaths(),
-    tanstackStart({
-      target: "vercel",
-    }),
-    react(),
+    tanstackStart(),
+    nitro(),
+    viteReact(),
   ],
 });
